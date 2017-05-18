aa567ccc6a44a0f9cdd0894b651cf1ff51d3bd11
sehun <shn413@gmail.com>
2017-05-18 09:08:01 +0900

    complete register and login

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var express = require('express');
 var app = express();
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
 var http = require('http').Server(app);
 // socket.io 서버로 업그레이드
 var io = require('socket.io')(http);
@@ -28,6 +29,9 @@ var _storage = multer.diskStorage({
 var upload = multer({
     storage: _storage
 });
+// 비번 암호화
+var bkfd2Password = require("pbkdf2-password");
+var hasher = bkfd2Password();
 // mysql module import
 var mysql = require('mysql');
 var conn = mysql.createConnection({
@@ -37,6 +41,9 @@ var conn = mysql.createConnection({
     database: 'o2'
 });
 conn.connect();
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
 
 app.locals.pretty = true;
 //public 디렉토리에 있는 정적 파일을 '/static'으로 사용
@@ -52,15 +59,170 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
+//mysql 과 session 연결
 app.use(session({
-  secret: 'sessisaffaolskafontest123',
-  resave: false,
-  saveUninitialized: true
+    //key: 'session_cookie_name',
+    secret: 'aswelrkke',
+    resave: false,
+    saveUninitialized: true,
+    store: new MySQLStore({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'ekqlscl135',
+        database: 'o2'
+    })
 }));
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.post('/auth/login', function(req, res){
-    res.send(req.body.username);
+// index.ejs 페이지 라우팅, data 목록 보여주기
+app.get(['/', '/content/:id'], function(req, res) {
+    // passport는 원래 req가 가지고 있지 않은 객체인 user객체를 req의 소속으로 만들어줌
+    // user는 deserializeUser의 done함수의 두번째 인자인 user로 부터 기인
+    if(req.user && req.user.displayName){
+        res.render('index',{displayName:req.user.displayName});
+    }else{
+        res.render('index',{displayName:''});
+    }
 });
+
+var users = [
+    {
+        authId:'123123',
+        username : 'sehun',
+        password : 'ixHP8/eq3ySKrsCI00YaREwquLAV0LSVwJiCppX5D2N6vzs0GgEHY+Afzw/cuCX1C9ClkqfmMb3E8rCKn3g1XXfC8VVL+D6U7dBpMp631v10CIKWe/uDSSfWnyQJ/EPXBno5+E9PctqLb50RJvp2YmzydEB7YYQn98SMrxyDwy4=',
+        salt : 'TjYXkWUtOsVflLFM0D8TS0r/EaPZqaYoj5SE2TrqKLH48Bd1DDadgACdqfbYklgTqCidt8axGD250vEKBrBnJw==',
+        displayName : '싸나이'
+    }
+];
+
+passport.serializeUser(function(user, done) {
+    // done함수의 두번째 인자로 user를 식별하는데 이 값이 세션에 저장된다.
+    done(null, user.authId);
+});
+// 두번째 로그인시 세션에 저장된 user.username이 id 값으로 들어감
+passport.deserializeUser(function(id, done) {
+    console.log('deserializeUser', id);
+    var sql = 'SELECT * FROM users WHERE authId=?';
+    conn.query(sql, [id], function(err, results){
+        if(!results[0]){
+            done('There is no user');
+        }else{
+            done(null, results[0]);
+        }
+    });
+});
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        var uname = username;
+        var pwd = password;
+        var sql = 'SELECT * FROM users WHERE authId=?';
+        conn.query(sql, ['local:'+uname], function(err, results){
+            if(!results[0]){
+                console.log(err);
+                return done('There is no user');
+            }
+            var user = results[0];
+            //입력한 비번과 저장되어있는 salt를 인자로 받음
+            return hasher({password:pwd, salt:user.salt}, function(err, pass, salt, hash){
+                if(hash === user.password){
+                    // 비번까지 맞으면, done함수의 인자인 user가 serializeUser로 넘어감
+                    console.log('LocalStrategy', user);
+                    done(null, user);
+                    // req.session.displayName = user.displayName;
+                    // req.session.save(function(){
+                    //     res.redirect('/');
+                    // });
+                }else{
+                    done(null, false);
+                    //res.send('누구? <a href="/auth/login">login</a>');
+                }
+            });
+        });
+    }
+));
+
+
+
+app.get('/auth/register', function(req, res){
+    res.render('register');
+});
+app.post('/auth/register', function(req, res){
+    hasher({password:req.body.password}, function(err, pass, salt, hash){
+        var user = {
+            authId: 'local:'+req.body.username,
+            username: req.body.username,
+            password: hash,
+            salt: salt,
+            displayName: req.body.displayName
+        };
+        // set? 를 통해 user객체로 사용자정보를 추가할 수 있다
+        var sql = 'INSERT INTO users SET ?';
+        conn.query(sql, user, function(err, results){
+            if(err){
+                console.log(err);
+                res.status(500);
+            }else{
+                // passport에서 처리하는 세션, 등록후 바로 로그인
+                req.login(user, function(err){
+                    req.session.save(function(){
+                        res.redirect('/');
+                    });
+                });
+            }
+        });
+    });
+});
+app.get('/auth/login', function(req, res){
+    res.render('login');
+});
+
+//passport 모듈을 이용해 직접 메일, 비번 입력으로 로그인
+app.post('/auth/login',
+    passport.authenticate(
+        'local',
+        {
+            successRedirect: '/',
+            failureRedirect: '/auth/login',
+            failureFlash: true
+        }
+    )
+);
+// app.post('/auth/login', function(req, res){
+//     var uname = req.body.username;
+//     var pwd = req.body.password;
+//     for(var i=0; i<users.length; i++){
+//         var user = users[i];
+//         if(user.username === uname){
+//             //입력한 비번과 저장되어있는 salt를 인자로 받음
+//             return hasher({password:pwd, salt:user.salt}, function(err, pass, salt, hash){
+//                 console.log(pass, salt, hash);
+//                 if(hash === user.password){
+//                     req.session.displayName = user.displayName;
+//                     req.session.save(function(){
+//                         res.redirect('/');
+//                     });
+//                 }else{
+//                     res.send('누구? <a href="/auth/login">login</a>');
+//                 }
+//             });
+//         }
+//     }
+// });
+app.get('/auth/logout', function(req, res){
+    //delete req.session.displayName;
+    // req.session.save(function(){
+    //     res.redirect('/');
+    // });
+    req.logout();
+    req.session.save(function(){
+        res.redirect('/');
+    });
+});
+
+
 app.get('/recoFee', function(req, res) {
     if(req.session.count){
         req.session.count++;
@@ -123,13 +285,6 @@ app.get('/myPage', function(req, res) {
     res.send('마이페이지');
 });
 
-// about.ejs 파일 render
-app.get('/about', function(req, res) {
-    res.render('about', {
-        name: 'Sehun',
-        age: '27'
-    });
-});
 
 // static file과 라우팅
 app.get('/route', function(req, res) {
@@ -167,10 +322,7 @@ app.post('/content/add', function(req, res) {
         }
     });
 });
-// index.ejs 페이지 라우팅, data 목록 보여주기
-app.get(['/', '/content/:id'], function(req, res) {
-    res.render('index');
-});
+
 // app.get(['/', '/content/:id'], function(req, res) {
 //     var allRows = 'select id, title from topic';
 //     conn.query(allRows, function(err, rows, fields) {
